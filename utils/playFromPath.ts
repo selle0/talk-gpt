207f83e7a5e4dcfa1a57554cb536ccd9ade043a8
selle0 <gisellemezazavala17@gmail.com>
2024-01-19 05:42:04 -0700

    todo el proyecto

---
+++ utils/playFromPath.ts
@@ -0,0 +1,11 @@
+import { Audio } from "expo-av";
+
+export const playFromPath = async (path: string) => {
+    try {
+        const soundObject = new Audio.Sound();
+        await soundObject.loadAsync({ uri: path });
+        await soundObject.playAsync();
+        } catch (e) {
+             console.log(e);
+        }
+};
